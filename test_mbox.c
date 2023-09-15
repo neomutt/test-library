7548db3c26a71b8c056ff515f1ea57c5c82aa24d
Richard Russon <rich@flatcap.org>
2023-09-16 00:43:32 +0100

    test_mbox

--- test_mbox.c
+++ test_mbox.c
@@ -5,52 +5,61 @@
 #include "mutt/lib.h"
 #include "email/email.h"
 #include "core/lib.h"
+#include "mutt.h"
 #include "mbox/lib.h"
+#include "progress/lib.h"
 #include "mx.h"
 
-struct Progress;
-
-bool C_Autocrypt;
-short C_Sort;
-short C_WriteInc;
-bool C_CheckMboxSize;
-bool C_MailCheckRecent;
 char *HomeDir;
 char *ShortHostname;
 char *Username;
-short C_ReadInc;
 volatile sig_atomic_t SigInt;
-struct Context *Context = NULL;
 
 typedef uint16_t CopyMessageFlags;
 typedef uint32_t CopyHeaderFlags;
-typedef uint16_t CompletionFlags;
 
-struct Mailbox *ctx_mailbox(struct Context *ctx)
+#define CONFIG_INIT_TYPE(CS, NAME)                                             \
+  extern const struct ConfigSetType Cst##NAME;                                 \
+  cs_register_type(CS, &Cst##NAME)
+
+void buf_pretty_mailbox(struct Buffer *buf)
 {
-  return ctx ? ctx->mailbox : NULL;
+}
+
+void mutt_set_flag(struct Mailbox *m, struct Email *e, enum MessageType flag,
+                   bool bf, bool upd_mbox)
+{
+}
+
+int mx_msg_close(struct Mailbox *m, struct Message **ptr)
+{
+  return -1;
+}
+
+struct Message *mx_msg_open(struct Mailbox *m, struct Email *e)
+{
+  return NULL;
+}
+
+void progress_free(struct Progress **ptr)
+{
+}
+
+struct Progress *progress_new(const char *msg, enum ProgressType type, size_t size)
+{
+  return NULL;
+}
+
+bool progress_update(struct Progress *progress, size_t pos, int percent)
+{
+  return false;
 }
 
 void nm_edata_free(void **ptr)
 {
 }
 
-void buf_mktemp_full(struct Buffer *buf, const char *prefix,
-                             const char *suffix, const char *src, int line)
-{
-}
-
-void buf_pretty_mailbox(struct Buffer *buf)
-{
-}
-
 int mutt_autocrypt_process_autocrypt_header(struct Email *e, struct Envelope *env)
-{
-  return -1;
-}
-
-int buf_get_field(const char *field, struct Buffer *buf, CompletionFlags complete,
-                          bool multiple, char ***files, int *numfiles)
 {
   return -1;
 }
@@ -60,79 +69,22 @@ int mutt_copy_message(FILE *fp_out, struct Mailbox *m, struct Email *e, CopyMess
   return -1;
 }
 
-struct Context *mx_mbox_open(struct Mailbox *m, OpenMailboxFlags flags)
+bool mx_mbox_open(struct Mailbox *m, OpenMailboxFlags flags)
 {
-  return NULL;
+  return true;
 }
 
-int mx_mbox_close(struct Context **pctx)
+enum MxStatus mx_mbox_close(struct Mailbox *m)
 {
-  return -1;
+  return MX_STATUS_OK;
 }
 
-int url_parse_mailto(struct Envelope *e, char **body, const char *src)
-{
-  return -1;
-}
-
-void mutt_clear_threads(struct Context *ctx)
-{
-  mutt_message("mutt_clear_threads NOTIMPL");
-}
-
-int mutt_copy_message_ctx(FILE *fpout, struct Context *src, struct Email *hdr, int flags, int chflags)
-{
-  mutt_message("mutt_copy_message_ctx NOTIMPL");
-  return 0;
-}
-
-struct Mailbox *mutt_mailbox_find(const char *path)
-{
-  mutt_message("mutt_mailbox_find NOTIMPL");
-  return NULL;
-}
-
-void mutt_make_label_hash(struct Context *ctx)
+void mutt_make_label_hash(struct Mailbox *m)
 {
   mutt_message("mutt_make_label_hash NOTIMPL");
 }
 
-void mutt_mktemp_full(char *s, size_t slen, const char *prefix, const char *suffix, const char *src, int line)
-{
-  mutt_message("mutt_mktemp_full NOTIMPL");
-}
-
-void mutt_pretty_mailbox(char *s, size_t buflen)
-{
-  mutt_message("mutt_pretty_mailbox NOTIMPL");
-}
-
-void mutt_progress_init(struct Progress *progress, const char *msg, unsigned short flags, unsigned short inc, size_t size)
-{
-  mutt_message("mutt_progress_init NOTIMPL");
-}
-
-void mutt_progress_update(struct Progress *progress, long pos, int percent)
-{
-  mutt_message("mutt_progress_update NOTIMPL");
-}
-
-void mutt_set_flag_update(struct Context *ctx, struct Email *h, int flag, bool bf, bool upd_ctx)
-{
-  mutt_message("mutt_set_flag_update NOTIMPL");
-}
-
-void mutt_sort_headers(struct Context *ctx, int init)
-{
-  mutt_message("mutt_sort_headers NOTIMPL");
-}
-
-void mutt_mailbox_update(struct Mailbox *m)
-{
-  mutt_message("mutt_mailbox_update NOTIMPL");
-}
-
-void mx_alloc_memory(struct Mailbox *m)
+void mx_alloc_memory(struct Mailbox *m, int req_size)
 {
   size_t s = MAX(sizeof(struct Email *), sizeof(int));
 
@@ -159,95 +111,63 @@ void mx_alloc_memory(struct Mailbox *m)
   }
 }
 
-void mx_fastclose_mailbox(struct Mailbox *m)
+void mx_fastclose_mailbox(struct Mailbox *m, bool keep_account)
 {
   mutt_message("mx_fastclose_mailbox NOTIMPL");
 }
 
-void mx_update_context(struct Context *ctx, int new_messages)
-{
-  mutt_message("mx_update_context NOTIMPL");
-}
-
-void mutt_get_stat_timespec(struct timespec *dest, struct stat *sb, enum MuttStatType type)
-{
-  dest->tv_sec = 0;
-  dest->tv_nsec = 0;
-
-  switch (type)
-  {
-    case MUTT_STAT_ATIME:
-      dest->tv_sec = sb->st_atime;
-#ifdef HAVE_STRUCT_STAT_ST_ATIM_TV_NSEC
-      dest->tv_nsec = sb->st_atim.tv_nsec;
-#endif
-      break;
-    case MUTT_STAT_MTIME:
-      dest->tv_sec = sb->st_mtime;
-#ifdef HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC
-      dest->tv_nsec = sb->st_mtim.tv_nsec;
-#endif
-      break;
-    case MUTT_STAT_CTIME:
-      dest->tv_sec = sb->st_ctime;
-#ifdef HAVE_STRUCT_STAT_ST_CTIM_TV_NSEC
-      dest->tv_nsec = sb->st_ctim.tv_nsec;
-#endif
-      break;
-  }
-}
-
-int mutt_timespec_compare(struct timespec *a, struct timespec *b)
-{
-  if (a->tv_sec < b->tv_sec)
-    return -1;
-  if (a->tv_sec > b->tv_sec)
-    return 1;
-
-  if (a->tv_nsec < b->tv_nsec)
-    return -1;
-  if (a->tv_nsec > b->tv_nsec)
-    return 1;
-  return 0;
-}
-
-int mutt_stat_timespec_compare(struct stat *sba, enum MuttStatType type, struct timespec *b)
-{
-  struct timespec a = { 0 };
-
-  mutt_get_stat_timespec(&a, sba, type);
-  return mutt_timespec_compare(&a, b);
-}
-
-enum MailboxType mx_path_probe(const char *path)
-{
-  mutt_message("mx_path_probe NOTIMPL");
-  return -1;
-}
-
 int main(int argc, char *argv[])
 {
+  static struct ConfigDef Vars[] = {
+    // clang-format off
+    { "assumed_charset", DT_SLIST|SLIST_SEP_COLON|SLIST_ALLOW_EMPTY, 0, 0, NULL, 0, 0 },
+    { "autocrypt", DT_BOOL, false, 0, NULL, 0, 0 },
+    { "charset", DT_STRING|DT_NOT_EMPTY|DT_CHARSET_SINGLE, IP "utf-8", 0, NULL, 0, 0},
+    { "maildir_field_delimiter", DT_STRING, IP ":", 0, NULL, 0, 0 },
+    { "reply_regex", DT_REGEX|R_INDEX|R_RESORT, IP "^((re|aw|sv)(\\[[0-9]+\\])*:[ \t]*)*", 0, NULL, 0, 0 },
+    { "send_charset", DT_SLIST|SLIST_SEP_COLON|SLIST_ALLOW_EMPTY|DT_CHARSET_STRICT, IP "us-ascii:iso-8859-1:utf-8", 0, NULL, },
+    { NULL },
+    // clang-format on
+  };
+
+  struct ConfigSet *cs = cs_new(50);
+  struct ConfigSubset *sub = cs_subset_new (NULL, NULL, NULL);
+  CONFIG_INIT_TYPE(cs, Bool);
+  CONFIG_INIT_TYPE(cs, Path);
+  CONFIG_INIT_TYPE(cs, Regex);
+  CONFIG_INIT_TYPE(cs, Slist);
+  CONFIG_INIT_TYPE(cs, String);
+
+  NeoMutt = neomutt_new(cs);
+
+  cs_register_variables(cs, Vars, DT_NO_FLAGS);
+
   char *file = "test.mbox";
-  struct Context ctx = { 0 };
-  struct Account a = { 0 };
-  struct Mailbox m = { 0 };
-  ctx.mailbox = &m;
-  m.type = MUTT_MBOX;
-  m.account = &a;
-  a.type = MUTT_MBOX;
-  m.pathbuf = buf_make(128);
-  buf_strcpy(&m.pathbuf, file);
+  struct Account *a = account_new(NULL, sub);
+  struct Mailbox *m = mailbox_new();
+  m->type = MUTT_MBOX;
+  m->account = a;
+  a->type = MUTT_MBOX;
+  m->pathbuf = buf_make(128);
+  buf_strcpy(&m->pathbuf, file);
 
-  int rc = MxMboxOps.mbox_open(ctx.mailbox);
+  int rc = MxMboxOps.mbox_open(m);
   printf("%d\n", rc);
   if (rc != 0)
     return 1;
 
-  rc = MxMboxOps.mbox_close(ctx.mailbox);
+  rc = MxMboxOps.mbox_close(m);
   printf("%d\n", rc);
   if (rc != 0)
     return 1;
 
-  buf_dealloc(&m.pathbuf);
+  config_cache_cleanup();
+  neomutt_free(&NeoMutt);
+  cs_subset_free(&sub);
+  cs_free(&cs);
+  buf_dealloc(&m->pathbuf);
+
+  mailbox_free(&m);
+  account_free(&a);
   return 0;
 }
